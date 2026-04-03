e5243649b6423f322ef3bffbf5d193ee012ae4cd
Samuel Franken <frankensamuel92@gmail.com>
2026-04-03 11:29:49 +0200

    added animation

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 
-from scripts.entities import PhysicsEntity
+from scripts.entities import PhysicsEntity, Player
 from scripts.utils import load_image, load_images, Animation
 from scripts.tilemap import Tilemap
 from scripts.clouds import Clouds
@@ -36,7 +36,7 @@ class Game:
 
         self.clouds = Clouds(self.assets['clouds'], count=16)
         
-        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        self.player = Player(self, (50, 50), (8, 15))
 
         self.tilemap = Tilemap(self, 16)
 
